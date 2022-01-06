d5054209e841b8eb95d3f5b8474cce765a521aed
darenbt <darenbt@gmail.com>
2022-01-06 00:36:39 -0600

    Repeat them all

---
+++ js/String Repeat.js
@@ -0,0 +1,9 @@
+//Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+console.log(repeatStr(3, "*") // shoul equal "***"
+console.log(repeatStr(5, "#") // shoul equal "#####"
+console.log(repeatStr(2, "ha ") // shoul equal "ha ha "
