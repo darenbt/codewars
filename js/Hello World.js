2b1d6dd6f789f6a3f0f6ac5c281ada6f37e2726c
darenbt <darenbt@gmail.com>
2022-02-02 22:38:00 -0600

    Created correct function.

---
+++ js/Hello World.js
@@ -0,0 +1,7 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+// My solution:
+const greet = () => 'hello world!'
+
+console.log(typeof greet); //should return "function"
+console.log(greet()); //should return  "hello world!"
