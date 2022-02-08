e287270b23a10e0a37a5a3ac915782fe3eff3ea9
darenbt <darenbt@gmail.com>
2022-02-07 23:30:02 -0600

    Found one.

---
+++ js/You Only Need One.js
@@ -0,0 +1,15 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//
+// Array can contain numbers or strings. X can be either.
+//
+// Return true if the array contains the value, false if not.
+
+// My solution:
+function check(a, x) {
+  return a.includes(x);
+}
+
+console.log(check([66, 101], 66)); // should return true
+console.log(check([101, 45, 75, 105, 99, 107], 107)); // should return true
+console.log(check(['t', 'e', 's', 't'], 'e')); // should return true
+console.log(check(['what', 'a', 'great', 'kata'], 'kat')); // should return false
