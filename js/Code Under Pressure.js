c61e99acfdf093cc225bd6d94459a529771b9904
darenbt <darenbt@gmail.com>
2022-03-12 00:06:10 -0600

    Became a diamond.

---
+++ js/Code Under Pressure.js
@@ -0,0 +1,11 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+// My solution:
+const doubleInteger = i => i*2
+
+// Test Cases:
+console.log(doubleInteger(2)); // should return 4
+console.log(doubleInteger(4)); // should return 8
+console.log(doubleInteger(-10)); // should return -20
+console.log(doubleInteger(0)); // should return 0
+console.log(doubleInteger(100)); // should return 200
