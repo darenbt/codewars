9574b2659f8b8e5deac883ef8fdea5f01a24deab
darenbt <darenbt@gmail.com>
2022-05-16 22:23:08 -0500

    Returned expected strings.

---
+++ js/Returning Strings.js
@@ -0,0 +1,9 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// My solution:
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+console.log(greet("Ryan"); // should return "Hello, Ryan how are you doing today?"
+console.log(greet("Shingles"); // should return "Hello, Shingles how are you doing today?"
