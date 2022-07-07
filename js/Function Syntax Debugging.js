5e836e02e34bdb582c83f681ee28f4406ac7c0fe
darenbt <darenbt@gmail.com>
2022-07-06 23:43:08 -0500

    Correctly debugged code.

---
+++ js/Function Syntax Debugging.js
@@ -0,0 +1,11 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// Supplied code:
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+// My solution:
+function main (verb, noun){
+  return verb + noun
+}
